d7c9a49304414d5322df65b70f8771f0cae8c478
allanfgodoi <allan.godoi@ufrgs.br>
2023-05-07 23:22:43 -0300

    Added both options to redefine and not

--- D0MesonsID/Skims/macro_divideTree_Rdataframe.py
+++ D0MesonsID/Skims/macro_divideTree_Rdataframe.py
@@ -1,9 +1,9 @@
-###To run it do: << pyroot macro_divideTree_Rdataframe.py >> 
+###To run it do: << python macro_divideTree_Rdataframe.py >> 
 
 import ROOT
 
 # We prepare an input tree to run on
-fileName = "/home/allanfgodoi/Desktop/tree_skim_MC_prompt.root"
+fileName = "/Users/cesarbernardes/Dropbox/Ubuntu_1204/AltasEnergias/ProfessorUFRGS/OrientacaoDeAlunos/IC_TCC/TopicosDeEstudo/D0_Selection/D0_MC_SkimmedTrees/tree_skim_MC_prompt.root"
 treeName = "Dfinder/ntDkpi"
 outFileNameTrain = "tree_skim_MC_promptTrain.root"
 outFileNameTest = "tree_skim_MC_promptTest.root"
@@ -11,7 +11,7 @@ outFileNameTest = "tree_skim_MC_promptTest.root"
 # We read the tree from the file and create a RDataFrame
 d = ROOT.RDataFrame(treeName, fileName) 
 
-# Auxiliar functions to Redefine variables as vector<float> instead of ROOT::VecOps::RVec<Float_t> default format from RDataframe
+# Auxiliar functions to define variables as vector<float> instead of ROOT::VecOps::RVec<Float_t> default format from RDataframe
 getVector_code ='''
 std::vector<float> getVectorFloat_Aux(ROOT::VecOps::RVec<Float_t> vec_){
    std::vector<float> v;
@@ -25,78 +25,78 @@ ROOT.gInterpreter.Declare(getVector_code)
 
 ##select first 500k entries 
 d_train = d.Range(500000) \
-           .Redefine("DPt","getVectorFloat_Aux(DPt)") \
-           .Redefine("DRapidity","getVectorFloat_Aux(DRapidity)") \
-           .Redefine("DMass","getVectorFloat_Aux(DMass)") \
-           .Redefine("DPhi","getVectorFloat_Aux(DPhi)") \
-           .Redefine("D2DDecayLength","getVectorFloat_Aux(D2DDecayLength)") \
-           .Redefine("DD0Err","getVectorFloat_Aux(DD0Err)") \
-           .Redefine("D3DDecayLength","getVectorFloat_Aux(D3DDecayLength)") \
-           .Redefine("DDxyzErr","getVectorFloat_Aux(DDxyzErr)") \
-           .Redefine("D3DDecayLengthSignificance","getVectorFloat_Aux(D3DDecayLengthSignificance)") \
-           .Redefine("D2DDecayLengthSignificance","getVectorFloat_Aux(D2DDecayLengthSignificance)") \
-           .Redefine("DVtxProb","getVectorFloat_Aux(DVtxProb)") \
-           .Redefine("D3DPointingAngle","getVectorFloat_Aux(D3DPointingAngle)") \
-           .Redefine("D2DPointingAngle","getVectorFloat_Aux(D2DPointingAngle)") \
-           .Redefine("DDca","getVectorFloat_Aux(DDca)") \
-           .Redefine("DImpactParameter3D","getVectorFloat_Aux(DImpactParameter3D)") \
-           .Redefine("DImpactParameter3Derror","getVectorFloat_Aux(DImpactParameter3Derror)") \
-           .Redefine("DTtrk1Pt","getVectorFloat_Aux(DTtrk1Pt)") \
-           .Redefine("DTrk2Pt","getVectorFloat_Aux(DTrk2Pt)") \
-           .Redefine("DTrk1PtErr","getVectorFloat_Aux(DTrk1PtErr)") \
-           .Redefine("DTrk2PtErr","getVectorFloat_Aux(DTrk2PtErr)") \
-           .Redefine("DTrk1Eta","getVectorFloat_Aux(DTrk1Eta)") \
-           .Redefine("DTrk2Eta","getVectorFloat_Aux(DTrk2Eta)") \
-           .Redefine("DxyDCASignificanceDaugther1","getVectorFloat_Aux(DxyDCASignificanceDaugther1)") \
-           .Redefine("DxyDCASignificanceDaugther2","getVectorFloat_Aux(DxyDCASignificanceDaugther2)") \
-           .Redefine("DzDCASignificanceDaugther1","getVectorFloat_Aux(DzDCASignificanceDaugther1)") \
-           .Redefine("DzDCASignificanceDaugther2","getVectorFloat_Aux(DzDCASignificanceDaugther2)") \
-           .Redefine("DTrk1Chi2n","getVectorFloat_Aux(DTrk1Chi2n)") \
-           .Redefine("DTrk2Chi2n","getVectorFloat_Aux(DTrk2Chi2n)") \
-           .Redefine("DGen","getVectorFloat_Aux(DGen)") \
-           .Redefine("DGenIndex","getVectorFloat_Aux(DGenIndex)") \
-           .Redefine("DGennDa","getVectorFloat_Aux(DGennDa)") \
-           .Redefine("DGenpt","getVectorFloat_Aux(DGenpt)") \
-           .Redefine("DGenphi","getVectorFloat_Aux(DGenphi)") \
-           .Redefine("DGeny","getVectorFloat_Aux(DGeny)") \
+           .Define("Dpt","getVectorFloat_Aux(DPt)") \
+           .Define("Drapidity","getVectorFloat_Aux(DRapidity)") \
+           .Define("Dmass","getVectorFloat_Aux(DMass)") \
+           .Define("Dphi","getVectorFloat_Aux(DPhi)") \
+           .Define("D2ddecayLength","getVectorFloat_Aux(D2DDecayLength)") \
+           .Define("Dd0Err","getVectorFloat_Aux(DD0Err)") \
+           .Define("D3ddecayLength","getVectorFloat_Aux(D3DDecayLength)") \
+           .Define("DdxyzErr","getVectorFloat_Aux(DDxyzErr)") \
+           .Define("D3ddecayLengthSignificance","getVectorFloat_Aux(D3DDecayLengthSignificance)") \
+           .Define("D2ddecayLengthSignificance","getVectorFloat_Aux(D2DDecayLengthSignificance)") \
+           .Define("DvtxProb","getVectorFloat_Aux(DVtxProb)") \
+           .Define("D3dpointingAngle","getVectorFloat_Aux(D3DPointingAngle)") \
+           .Define("D2dpointingAngle","getVectorFloat_Aux(D2DPointingAngle)") \
+           .Define("Ddca","getVectorFloat_Aux(DDca)") \
+           .Define("DimpactParameter3D","getVectorFloat_Aux(DImpactParameter3D)") \
+           .Define("DimpactParameter3Derror","getVectorFloat_Aux(DImpactParameter3Derror)") \
+           .Define("Dtrk1Pt","getVectorFloat_Aux(DTtrk1Pt)") \
+           .Define("Dtrk2Pt","getVectorFloat_Aux(DTrk2Pt)") \
+           .Define("Dtrk1PtErr","getVectorFloat_Aux(DTrk1PtErr)") \
+           .Define("Dtrk2PtErr","getVectorFloat_Aux(DTrk2PtErr)") \
+           .Define("Dtrk1Eta","getVectorFloat_Aux(DTrk1Eta)") \
+           .Define("Dtrk2Eta","getVectorFloat_Aux(DTrk2Eta)") \
+           .Define("DxydcaSignificanceDaugther1","getVectorFloat_Aux(DxyDCASignificanceDaugther1)") \
+           .Define("DxydcaSignificanceDaugther2","getVectorFloat_Aux(DxyDCASignificanceDaugther2)") \
+           .Define("DzdcaSignificanceDaugther1","getVectorFloat_Aux(DzDCASignificanceDaugther1)") \
+           .Define("DzdcaSignificanceDaugther2","getVectorFloat_Aux(DzDCASignificanceDaugther2)") \
+           .Define("Dtrk1Chi2n","getVectorFloat_Aux(DTrk1Chi2n)") \
+           .Define("Dtrk2Chi2n","getVectorFloat_Aux(DTrk2Chi2n)") \
+           .Define("Dgen","getVectorFloat_Aux(DGen)") \
+           .Define("DgenIndex","getVectorFloat_Aux(DGenIndex)") \
+           .Define("DgennDa","getVectorFloat_Aux(DGennDa)") \
+           .Define("Dgenpt","getVectorFloat_Aux(DGenpt)") \
+           .Define("Dgenphi","getVectorFloat_Aux(DGenphi)") \
+           .Define("Dgeny","getVectorFloat_Aux(DGeny)") \
 
 ##from 500001 till the end of dataset, which has around 1M events
 d_test = d.Range(500001,0) \
-          .Redefine("DPt","getVectorFloat_Aux(DPt)") \
-          .Redefine("DRapidity","getVectorFloat_Aux(DRapidity)") \
-          .Redefine("DMass","getVectorFloat_Aux(DMass)") \
-          .Redefine("DPhi","getVectorFloat_Aux(DPhi)") \
-          .Redefine("D2DDecayLength","getVectorFloat_Aux(D2DDecayLength)") \
-          .Redefine("DD0Err","getVectorFloat_Aux(DD0Err)") \
-          .Redefine("D3DDecayLength","getVectorFloat_Aux(D3DDecayLength)") \
-          .Redefine("DDxyzErr","getVectorFloat_Aux(DDxyzErr)") \
-          .Redefine("D3DDecayLengthSignificance","getVectorFloat_Aux(D3DDecayLengthSignificance)") \
-          .Redefine("D2DDecayLengthSignificance","getVectorFloat_Aux(D2DDecayLengthSignificance)") \
-          .Redefine("DVtxProb","getVectorFloat_Aux(DVtxProb)") \
-          .Redefine("D3DPointingAngle","getVectorFloat_Aux(D3DPointingAngle)") \
-          .Redefine("D2DPointingAngle","getVectorFloat_Aux(D2DPointingAngle)") \
-          .Redefine("DDca","getVectorFloat_Aux(DDca)") \
-          .Redefine("DImpactParameter3D","getVectorFloat_Aux(DImpactParameter3D)") \
-          .Redefine("DImpactParameter3Derror","getVectorFloat_Aux(DImpactParameter3Derror)") \
-          .Redefine("DTtrk1Pt","getVectorFloat_Aux(DTtrk1Pt)") \
-          .Redefine("DTrk2Pt","getVectorFloat_Aux(DTrk2Pt)") \
-          .Redefine("DTrk1PtErr","getVectorFloat_Aux(DTrk1PtErr)") \
-          .Redefine("DTrk2PtErr","getVectorFloat_Aux(DTrk2PtErr)") \
-          .Redefine("DTrk1Eta","getVectorFloat_Aux(DTrk1Eta)") \
-          .Redefine("DTrk2Eta","getVectorFloat_Aux(DTrk2Eta)") \
-          .Redefine("DxyDCASignificanceDaugther1","getVectorFloat_Aux(DxyDCASignificanceDaugther1)") \
-          .Redefine("DxyDCASignificanceDaugther2","getVectorFloat_Aux(DxyDCASignificanceDaugther2)") \
-          .Redefine("DzDCASignificanceDaugther1","getVectorFloat_Aux(DzDCASignificanceDaugther1)") \
-          .Redefine("DzDCASignificanceDaugther2","getVectorFloat_Aux(DzDCASignificanceDaugther2)") \
-          .Redefine("DTrk1Chi2n","getVectorFloat_Aux(DTrk1Chi2n)") \
-          .Redefine("DTrk2Chi2n","getVectorFloat_Aux(DTrk2Chi2n)") \
-          .Redefine("DGen","getVectorFloat_Aux(DGen)") \
-          .Redefine("DGenIndex","getVectorFloat_Aux(DGenIndex)") \
-          .Redefine("DGennDa","getVectorFloat_Aux(DGennDa)") \
-          .Redefine("DGenpt","getVectorFloat_Aux(DGenpt)") \
-          .Redefine("DGenphi","getVectorFloat_Aux(DGenphi)") \
-          .Redefine("DGeny","getVectorFloat_Aux(DGeny)") \
+          .Define("Dpt","getVectorFloat_Aux(DPt)") \
+          .Define("Drapidity","getVectorFloat_Aux(DRapidity)") \
+          .Define("Dmass","getVectorFloat_Aux(DMass)") \
+          .Define("Dphi","getVectorFloat_Aux(DPhi)") \
+          .Define("D2ddecayLength","getVectorFloat_Aux(D2DDecayLength)") \
+          .Define("Dd0Err","getVectorFloat_Aux(DD0Err)") \
+          .Define("D3ddecayLength","getVectorFloat_Aux(D3DDecayLength)") \
+          .Define("DdxyzErr","getVectorFloat_Aux(DDxyzErr)") \
+          .Define("D3ddecayLengthSignificance","getVectorFloat_Aux(D3DDecayLengthSignificance)") \
+          .Define("D2ddecayLengthSignificance","getVectorFloat_Aux(D2DDecayLengthSignificance)") \
+          .Define("DvtxProb","getVectorFloat_Aux(DVtxProb)") \
+          .Define("D3dpointingAngle","getVectorFloat_Aux(D3DPointingAngle)") \
+          .Define("D2dpointingAngle","getVectorFloat_Aux(D2DPointingAngle)") \
+          .Define("Ddca","getVectorFloat_Aux(DDca)") \
+          .Define("DimpactParameter3D","getVectorFloat_Aux(DImpactParameter3D)") \
+          .Define("DimpactParameter3Derror","getVectorFloat_Aux(DImpactParameter3Derror)") \
+          .Define("Dtrk1Pt","getVectorFloat_Aux(DTtrk1Pt)") \
+          .Define("Dtrk2Pt","getVectorFloat_Aux(DTrk2Pt)") \
+          .Define("Dtrk1PtErr","getVectorFloat_Aux(DTrk1PtErr)") \
+          .Define("Dtrk2PtErr","getVectorFloat_Aux(DTrk2PtErr)") \
+          .Define("Dtrk1Eta","getVectorFloat_Aux(DTrk1Eta)") \
+          .Define("Dtrk2Eta","getVectorFloat_Aux(DTrk2Eta)") \
+          .Define("DxydcaSignificanceDaugther1","getVectorFloat_Aux(DxyDCASignificanceDaugther1)") \
+          .Define("DxydcaSignificanceDaugther2","getVectorFloat_Aux(DxyDCASignificanceDaugther2)") \
+          .Define("DzdcaSignificanceDaugther1","getVectorFloat_Aux(DzDCASignificanceDaugther1)") \
+          .Define("DzdcaSignificanceDaugther2","getVectorFloat_Aux(DzDCASignificanceDaugther2)") \
+          .Define("Dtrk1Chi2n","getVectorFloat_Aux(DTrk1Chi2n)") \
+          .Define("Dtrk2Chi2n","getVectorFloat_Aux(DTrk2Chi2n)") \
+          .Define("Dgen","getVectorFloat_Aux(DGen)") \
+          .Define("DgenIndex","getVectorFloat_Aux(DGenIndex)") \
+          .Define("DgennDa","getVectorFloat_Aux(DGennDa)") \
+          .Define("Dgenpt","getVectorFloat_Aux(DGenpt)") \
+          .Define("Dgenphi","getVectorFloat_Aux(DGenphi)") \
+          .Define("Dgeny","getVectorFloat_Aux(DGeny)") \
 
-d_train.Snapshot(treeName, outFileNameTrain, {"centrality","PVx","PVy","PVz","DPt","DRapidity","DMass","DPhi","D2DDecayLength","DD0Err","D3DDecayLength","DDxyzErr","D3DDecayLengthSignificance","D2DDecayLengthSignificance","DVtxProb","D3DPointingAngle","D2DPointingAngle","DDca","DImpactParameter3D","DImpactParameter3Derror","DTtrk1Pt","DTrk2Pt","DTrk1PtErr","DTrk2PtErr","DTrk1Eta","DTrk2Eta","DxyDCASignificanceDaugther1","DxyDCASignificanceDaugther2","DzDCASignificanceDaugther1","DzDCASignificanceDaugther2","DTrk1Chi2n","DTrk2Chi2n","DGen","DGenIndex","DGennDa","DGenpt","DGenphi","DGeny"})
+d_train.Snapshot(treeName, outFileNameTrain, {"centrality","PVx","PVy","PVz","Dpt","Drapidity","Dmass","Dphi","D2ddecayLength","Dd0Err","D3ddecayLength","DdxyzErr","D3ddecayLengthSignificance","D2ddecayLengthSignificance","DvtxProb","D3dpointingAngle","D2dpointingAngle","Ddca","DimpactParameter3D","DimpactParameter3Derror","Dtrk1Pt","Dtrk2Pt","Dtrk1PtErr","Dtrk2PtErr","Dtrk1Eta","Dtrk2Eta","DxydcaSignificanceDaugther1","DxydcaSignificanceDaugther2","DzdcaSignificanceDaugther1","DzdcaSignificanceDaugther2","Dtrk1Chi2n","Dtrk2Chi2n","Dgen","DgenIndex","DgennDa","Dgenpt","Dgenphi","Dgeny"})
 
-d_test.Snapshot(treeName, outFileNameTest, {"centrality","PVx","PVy","PVz","DPt","DRapidity","DMass","DPhi","D2DDecayLength","DD0Err","D3DDecayLength","DDxyzErr","D3DDecayLengthSignificance","D2DDecayLengthSignificance","DVtxProb","D3DPointingAngle","D2DPointingAngle","DDca","DImpactParameter3D","DImpactParameter3Derror","DTtrk1Pt","DTrk2Pt","DTrk1PtErr","DTrk2PtErr","DTrk1Eta","DTrk2Eta","DxyDCASignificanceDaugther1","DxyDCASignificanceDaugther2","DzDCASignificanceDaugther1","DzDCASignificanceDaugther2","DTrk1Chi2n","DTrk2Chi2n","DGen","DGenIndex","DGennDa","DGenpt","DGenphi","DGeny"})
+d_test.Snapshot(treeName, outFileNameTest, {"centrality","PVx","PVy","PVz","Dpt","Drapidity","Dmass","Dphi","D2ddecayLength","Dd0Err","D3ddecayLength","DdxyzErr","D3ddecayLengthSignificance","D2ddecayLengthSignificance","DvtxProb","D3dpointingAngle","D2dpointingAngle","Ddca","DimpactParameter3D","DimpactParameter3Derror","Dtrk1Pt","Dtrk2Pt","Dtrk1PtErr","Dtrk2PtErr","Dtrk1Eta","Dtrk2Eta","DxydcaSignificanceDaugther1","DxydcaSignificanceDaugther2","DzdcaSignificanceDaugther1","DzdcaSignificanceDaugther2","Dtrk1Chi2n","Dtrk2Chi2n","Dgen","DgenIndex","DgennDa","Dgenpt","Dgenphi","Dgeny"})
