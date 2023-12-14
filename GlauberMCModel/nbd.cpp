f6e12c4a2c6a701858d5f368ae9aebcc14178692
Nicolas Correa <61383781+MiSalocin@users.noreply.github.com>
2023-12-14 16:40:48 -0300

    Commenting NBD

--- GlauberMCModel/nbd.cpp
+++ GlauberMCModel/nbd.cpp
@@ -15,13 +15,12 @@ using namespace std;
 // Structures used to generate and save data.
 struct Data {int nCol, nPart; double d;};
 
-void nbd(int         nruns = 1,
-            const char* dataFile = "./data.old.root",
-            const char* compareFile = "./HFSumEt.root"){
+void nbd(const char* dataFile = "./data.root",
+         const char* compareFile = "./HFSumEt.root"){
 
     double hScale = 1./1000.;
-    const int scanSize = 15;
-    const int nDim = 50;
+    const int scanSize = 5;
+    const int nDim = 10;
 
     double kMin = 0.3;
     double kMax = 0.7;
@@ -54,25 +53,22 @@ void nbd(int         nruns = 1,
         nCol.emplace_back(data.nCol);
     }
 
-//    NBD->SetParameters(1.3925, 0.4525);
-//    TH1F *generated = new TH1F("Generated", "Energia gerada;#Sigma E_{T}", 100, 0, 5);
-//    for (int col : nCol) {
-//        double sumET = 0;
-//        for (int n = 0; n < col; n++) {
-//            sumET += NBD->GetRandom();
-//        }
-//        generated->Fill(sumET * hScale);
-//    }
-    double k, mu;
+    // Create graphing variables
+    double bestK, bestMu;
     double chi2best = INFINITY;
+
+    // Run through the Mu values
     for (int iMu = 0; iMu < scanSize; iMu++) {
-        mu = muMin + muDelta * iMu;
+        double mu = muMin + muDelta * iMu;
+
+        // Run through the K values
         for (int ik = 0; ik < scanSize; ik++) {
-            k = kMin + kDelta * ik;
+            double localChi2best = INFINITY;
+            double k = kMin + kDelta * ik;
 
             NBD->SetParameters(mu, k);
-
             TH1F *generated = new TH1F("Generated", "sumETf vs events", 100, 0, 5);
+
             // Generate the random data
             for (int col : nCol) {
                 double sumET = 0;
@@ -81,32 +77,39 @@ void nbd(int         nruns = 1,
                 }
                 generated->Fill(sumET * hScale);
             }
+
+            // Calculate the Chi^2 ofr the given Mu and K
+            // through a range of normalization values
             double chi2min;
             double n;
             for (int iNorm = 0; iNorm < nDim; iNorm++) {
                 generated->Scale(nMin + nDelta * iNorm);
                 chi2min = 0;
                 double Observed, Expected, diff;
-                for (int i = 10; i < 100; i++) {
+                for (int i = 20; i < 100; i++) {
                     Expected = (double)exp->GetBinContent(i);
                     Observed = (double)generated->GetBinContent(i);
                     diff = Observed - Expected;
                     chi2min += (diff*diff)/Expected;
                 }
-                if (chi2best > chi2min) {
-                    chi2best = chi2min;
+                if (localChi2best > chi2min) {
+                    localChi2best = chi2min;
                     n = nMin + nDelta * iNorm;
                 }
                 generated->Scale(1/(nMin + nDelta * iNorm));
             }
-            results->SetBinContent(iMu + 1, ik + 1, chi2best);
+
+            // Output the resulting values in the terminal
+            results->SetBinContent(iMu + 1, ik + 1, localChi2best);
             cout
-                    << "Chi2 = " << chi2best
+                    << "Chi2 = " << localChi2best
                     << " MU = " << mu
                     << " K = " << k
                     << " N = " << n << endl;
-            chi2best = INFINITY;
 
+            // Saves the best K and Mu values
+            if (localChi2best < chi2best) {bestK = k; bestMu = mu;}
+            localChi2best = INFINITY;
             delete generated;
         }
     }
@@ -114,17 +117,11 @@ void nbd(int         nruns = 1,
     // Config the canvas
     auto *c = new TCanvas("canvas", "canvas", 1000, 743);
     c->SetLogy();
+    c->SetLogz();
     gStyle->SetPalette(kBird);
     gStyle->SetOptStat(0);
-//    generated->Scale(0.021);
-//    generated->SetLineColor(kGreen);
-//    generated->Draw("HIST");
-//    exp->Draw("same E0");
-//    auto* fitLeg = new TLegend(0.60,0.80,0.85,0.88);
-//    fitLeg->SetTextSize(0.025);
-//    fitLeg->AddEntry(generated, "Simulacao");
-//    fitLeg->AddEntry(exp, "Dados experimentais");
-//    fitLeg->Draw();
+
+    // Personalize resulting Chi2h histogram and draw it
     results->GetYaxis()->SetTitle("k");
     results->GetXaxis()->SetTitle("#mu");
     results->GetYaxis()->SetNdivisions(2*scanSize+1,0,0);
@@ -133,4 +130,29 @@ void nbd(int         nruns = 1,
     results->DrawClone("colz");
     results->Draw("same text");
     c->SaveAs("ue.png");
+
+
+    // Generate a new histogram with the best variable's values
+    NBD->SetParameters(bestMu, bestK);
+    TH1F *generated = new TH1F("Generated", "Energia gerada;#Sigma E_{T}", 100, 0, 5);
+    for (int col : nCol) {
+        double sumET = 0;
+        for (int n = 0; n < col; n++) {
+            sumET += NBD->GetRandom();
+        }
+        generated->Fill(sumET * hScale);
+    }
+
+    // Personalize the generated with the experimental histograms and draw them
+    generated->Scale(0.021);
+    generated->SetLineColor(kGreen);
+    generated->Draw("HIST");
+    exp->Draw("same E0");
+    auto* fitLeg = new TLegend(0.60,0.80,0.85,0.88);
+    fitLeg->SetTextSize(0.025);
+    fitLeg->AddEntry(generated, "Simulacao");
+    fitLeg->AddEntry(exp, "Dados experimentais");
+    fitLeg->Draw();
+    c->SaveAs("results.png");   
+
 }
